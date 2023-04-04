9d5f1afbfebcd5bf4ec4f83015716d00e956dea7
lucastorres <lucastorresgomes@gmail.com>
2023-04-04 03:39:12 -0300

    create user profile and get user repos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/layout/Footer';
 import Alert from './components/layout/Alert';
 import Home from './pages/Home';
 import About from './pages/About';
+import User from './pages/User';
 import NotFound from './pages/NotFound';
 import { GithubProvider } from './context/github/GithubContext';
 import { AlertProvider } from './context/alert/AlertContext';
@@ -18,12 +19,13 @@ function App() {
 
                         <main className='container mx-auto px-3 pb-12'>
                             <Alert />
-                                <Routes>
-                                    <Route path='/' element={<Home />} />
-                                    <Route path='/about' element={<About />} />
-                                    <Route path='/notfound' element={<NotFound />} />
-                                    <Route path='/*' element={<NotFound />} />
-                                </Routes>
+                            <Routes>
+                                <Route path='/' element={<Home />} />
+                                <Route path='/about' element={<About />} />
+                                <Route path='/user/:login' element={<User />} />
+                                <Route path='/notfound' element={<NotFound />} />
+                                <Route path='/*' element={<NotFound />} />
+                            </Routes>
                         </main>
 
                         <Footer/>
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,8 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
+        user: {},
+        repos: [],
         loading: false
     }
 
@@ -30,12 +32,57 @@ export const GithubProvider = ({children}) => {
 
         const {items} = await response.json()
 
-
         dispatch({
             type: 'GET_USERS',
             payload: items,
         })
     }
+
+    // get single user
+    const getUser = async (login) => {
+        setLoading()
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}`,
+        {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`,
+            },
+        })
+
+        if(response.status === 404) {
+            window.location = '/notfound'
+        } else {
+            const data = await response.json()   
+    
+            dispatch({
+                type: 'GET_USER',
+                payload: data,
+            })
+        }
+    }
+
+        // get user repos
+        const getUserRepos = async (login) => {
+            setLoading()
+
+            const params = new URLSearchParams({
+                sort: 'created',
+                per_page: 10,
+            })
+    
+            const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+                headers: {
+                    Authorization: `token ${GITHUB_TOKEN}`,
+                },
+            })
+    
+            const data = await response.json()
+    
+            dispatch({
+                type: 'GET_REPOS',
+                payload: data,
+            })
+        }
 
     // Clear users from state
     const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
@@ -48,8 +95,12 @@ export const GithubProvider = ({children}) => {
             value={{
                 users: state.users,
                 loading: state.loading,
+                user: state.user,
+                repos: state.repos,
                 searchUsers,
-                clearUsers
+                clearUsers,
+                getUser,
+                getUserRepos,
             }}
         >
             {children}
